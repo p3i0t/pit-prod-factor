b805d6bc7587f76a4956e60300087f2f805f3a2f
p3i0t <wangxin_0716@me.com>
2024-03-14 14:41:26 +0800

    add prod.py for prod related functions

--- pit/__init__.py
+++ pit/__init__.py
@@ -1,6 +1,6 @@
 """Pit: A Python package for printing money.
 """
-from pit.configs import get_training_config, get_inference_config, ProdsAvailable, list_prods, get_bars # noqa
+from pit.prod import get_training_config, get_inference_config, ProdsAvailable, list_prods, get_bars # noqa
 from pit.train import TrainPipeline  # noqa
 from pit.inference import InferencePipeline  # noqa
 
