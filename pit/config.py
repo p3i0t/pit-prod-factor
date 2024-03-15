531fdc999a2379047c3647a62afea3ee13bf0c73
p3i0t <wangxin_0716@me.com>
2024-03-16 01:31:51 +0800

    update config.yml

--- pit/config.py
+++ pit/config.py
@@ -26,16 +26,16 @@ def init_config() -> str:
             # all the raw data items
             "raw": {
                 "dir": "${pit_dir}/raw", 
-                "items": ["bar_1m", "univ", "barra", "ohlcv_1m", "return", "lag_return"],
+                "dataitems": ["bar_1m", "univ", "barra", "ohlcv_1m", "return", "lag_return"],
             },
             # all the derived data items
             "derived": {
                 "dir": "${pit_dir}/derived",
-                "items": ["bar_10m"]
+                "dataitems": ["bar_10m"]
             },
             "dataset": {
                 "dir": "${pit_dir}/dataset",
-                "items": ["10m_v2"],
+                "dataitems": ["10m_v2"],
             },
             "save_dir": "${pit_dir}/runs",
             "infer_dir": "${pit_dir}/infer",
@@ -48,13 +48,13 @@ def init_config() -> str:
         OmegaConf.resolve(cfg)
         from pathlib import Path
         Path(cfg.raw.dir).mkdir(parents=True, exist_ok=True)
-        for item in cfg.raw.items:
+        for item in cfg.raw.dataitems:
             Path(f"{cfg.raw.dir}").joinpath(item).mkdir(parents=True, exist_ok=True)
         Path(cfg.derived.dir).mkdir(parents=True, exist_ok=True)
-        for item in cfg.derived.items:
+        for item in cfg.derived.dataitems:
             Path(f"{cfg.derived.dir}").joinpath(item).mkdir(parents=True, exist_ok=True)
         Path(cfg.dataset.dir).mkdir(parents=True, exist_ok=True)
-        for item in cfg.dataset.items:
+        for item in cfg.dataset.dataitems:
             Path(f"{cfg.dataset.dir}").joinpath(item).mkdir(parents=True, exist_ok=True)
         Path(cfg.save_dir).mkdir(parents=True, exist_ok=True)
         Path(cfg.infer_dir).mkdir(parents=True, exist_ok=True)
