686e3e463143a7e91f03efd107023f8fbc6d9cec
p3i0t <wangxin_0716@me.com>
2024-07-04 16:11:31 +0800

    [CLI] remove unused cli

--- pit/cli.py
+++ pit/cli.py
@@ -3,47 +3,39 @@ import os
 from pathlib import Path
 
 import click
-from loguru import logger
-from omegaconf import OmegaConf
 import polars as pl
+from loguru import logger
 
 from pit import (
-    list_prods,
-    get_bars,
-    get_training_config,
-    get_inference_config,
-    TrainPipeline,
     InferencePipeline,
-)
-from pit.utils import any2ymd, any2date
-from pit.download import (
-    download_tcalendar,
-    download_stock_minute,
-    download_universe,
-    download_ohlcv_minute,
-    download_return,
-    download_lag_return,
-    download_stock_tick,
-    download_barra,
+    TrainPipeline,
+    get_bars,
+    get_inference_config,
+    get_training_config,
+    list_prods,
 )
 from pit.config import read_config
+from pit.download import (
+    download_lag_return,
+    download_return,
+    download_stock_minute,
+    download_tcalendar,
+    download_universe,
+)
 from pit.tcalendar import (
-    is_trading_day,
     adjust_date,
-    adjust_tcalendar_slot_df,
     get_tcalendar_df,
+    is_trading_day,
     load_tcalendar_list,
 )
+from pit.utils import any2date, any2ymd
 
 # data of these tasks is stored as one file per date. 
 tasks_dict = {
     "bar_1m": download_stock_minute,
-    "ohlcv_1m": download_ohlcv_minute,
-    'tick': download_stock_tick,
     "univ": download_universe,
     "return": download_return,
     "lag_return": download_lag_return,
-    'barra': download_barra,
 }
 
 
@@ -118,7 +110,7 @@ def _run_download_for_one_task(begin, end, task_name: str, verbose: bool = True,
         click.echo(f"{len(left_dates)} tasks to be done.")
 
 
-    if task_name in ['tick', 'bar_1m', 'ohlcv_1m']:
+    if task_name == "bar_1m":
         # fetch data in parallel on a daily basis since data is large.
         import ray
         n_jobs = ray_kwargs.get('n_jobs', 10)
@@ -187,7 +179,7 @@ def _run_download_for_one_task(begin, end, task_name: str, verbose: bool = True,
     "-t",
     "task_name",
     default="ohlcv_1m",
-    type=click.Choice(["return", "lag_return", "ohlcv_1m", "univ", "barra", "tick", "bar_1m", "all"]),
+    type=click.Choice(["return", "lag_return", "univ", "bar_1m", "all"]),
 )
 @click.option("--verbose", "-v", is_flag=True, help="whether to print details.")
 def download_task(begin, end, task_name, verbose):
@@ -202,75 +194,6 @@ def download_task(begin, end, task_name, verbose):
     for task in tasks:
         _run_download_for_one_task(begin, end, task, verbose=verbose)
     
-
-@click.command()
-@click.option(
-    "--n_jobs", default=10, type=int, help="number of parallel jobs are most."
-)
-@click.option(
-    "--cpus_per_task", "n_cpu", default=2, type=int, help="number of cpus per task."
-)
-@click.option("--verbose", "-v", is_flag=True, help="whether to print progress.")
-def long2widev2(n_jobs, n_cpu, verbose):
-    """Long to wide for v2 bars."""
-    import re
-    import ray
-    import itertools
-
-    cfg = read_config()
-    dir = Path(cfg.raw.dir)
-    src_dir = dir.joinpath("bar_1m")
-    tgt_dir = dir.joinpath("bar_1m_wide_v2")
-    if verbose is True:
-        click.echo(f"long2wide from {src_dir} to {tgt_dir}")
-    tgt_dir.mkdir(parents=True, exist_ok=True)
-
-    src_dates = [re.findall(r"\d{4}-\d{2}-\d{2}", d)[0] for d in os.listdir(src_dir)]
-    tgt_dates = [re.findall(r"\d{4}-\d{2}-\d{2}", d)[0] for d in os.listdir(tgt_dir)]
-    trading_dates = sorted(set(src_dates) - set(tgt_dates))
-    if verbose is True:
-        click.echo(f"{trading_dates=}")
-    click.echo(f"Long to wide {len(trading_dates)} tasks to be done.")
-
-    ray.init(num_cpus=n_jobs * n_cpu, ignore_reinit_error=True, include_dashboard=False)
-
-    @ray.remote(max_calls=1)
-    def long2wide_fn(date: str):
-        if verbose is True:
-            click.echo(f"running on {date}")
-        df = pl.scan_parquet(f"{src_dir}/{date}.parq")
-        cols = get_bars("v2")
-        df = df.with_columns(pl.col("time").dt.strftime("%H%M").alias("slot"))
-        df = df.with_columns([pl.col(_c).cast(pl.Float32) for _c in cols]).collect()
-        slots = df.get_column("slot").unique().sort().to_list()
-        df = df.pivot(index=["symbol", "date"], columns="slot", values=cols)
-        name_mapping = {
-            f"{col}_slot_{slt}": f"{col}_{slt}"
-            for col, slt in itertools.product(cols, slots)
-        }
-        df = df.rename(name_mapping)
-        df.write_parquet(f"{tgt_dir}/{date}.parq")
-        return date
-
-    task_ids = []
-    n_task_finished = 0
-    for exp_id, _d in enumerate(trading_dates, 1):
-        if verbose is True:
-            click.echo(f"running on {_d}")
-        task_id = long2wide_fn.options(
-            name="x",
-            num_cpus=n_cpu,
-        ).remote(date=_d)
-        task_ids.append(task_id)
-
-        if len(task_ids) >= n_jobs:
-            dones, task_ids = ray.wait(task_ids, num_returns=1)
-            ray.get(dones)
-            n_task_finished += 1
-            logger.info(f"{n_task_finished} tasks finished.")
-    ray.get(task_ids)
-    click.echo("task long2wide_v2 done.")
-
 
 @click.command()
 @click.option("--n_jobs", default=10, type=int, help="number of parallel jobs.")
@@ -290,8 +213,9 @@ def downsample10(n_jobs, n_cpu, verbose):
     if verbose is True:
         click.echo(f"downsample from {src_dir} to {tgt_dir}")
     Path(tgt_dir).mkdir(parents=True, exist_ok=True)
-    import ray
     import re
+
+    import ray
 
     @ray.remote(max_calls=3)
     def _downsample(file):
@@ -338,19 +262,23 @@ def downsample10(n_jobs, n_cpu, verbose):
 @click.option(
     "--cpu_per_task", "n_cpu", default=2, type=int, help="number of cpus per task."
 )
-def merge10_v2(n_jobs, n_cpu):
+def generate_dataset(n_jobs, n_cpu):
     cfg = read_config()
     dir_10m = Path(cfg.derived.dir).joinpath('bar_10m')
+    dir_1m = Path(cfg.raw.dir).joinpath('bar_1m')
     dir_univ = Path(cfg.raw.dir).joinpath('univ')
     dir_ret = Path(cfg.raw.dir).joinpath('return')
     dir_lag_ret = Path(cfg.raw.dir).joinpath('lag_return')
 
     tgt_dir = Path(cfg.dataset.dir).joinpath('10m_v2')
-    from collections import defaultdict
-    import re
-    import ray
     import itertools
+    import re
+    from collections import defaultdict
+
+    import ray
     from dlkit.utils import get_time_slots
+
+    from pit.downsample import downsample_1m_to_10m
 
     bars = get_bars("v2")
     slots = get_time_slots("0930", "1500", freq_in_min=10)
@@ -393,11 +321,11 @@ def merge10_v2(n_jobs, n_cpu):
             df_list = []
             for _d in dates:
                 df_univ = pl.scan_parquet(f"{dir_univ}/{_d}.parq").collect()
-                df_10m = (
-                    pl.scan_parquet(f"{dir_10m}/{_d}.parq")
-                    .select(["date", "symbol"] + v2_cols)
-                    .collect()
+                bars = get_bars("v2")
+                df_10m = downsample_1m_to_10m(
+                    pl.scan_parquet(f"{dir_1m}/{_d}.parq"), bars=bars
                 )
+                df_10m = df_10m.select(["date", "symbol"] + v2_cols)
                 df_10m = df_10m.with_columns(pl.col("symbol").cast(pl.Categorical))
                 df_ret = pl.scan_parquet(f"{dir_ret}/{_d}.parq").collect()
                 df_lag_ret = pl.scan_parquet(f"{dir_lag_ret}/{_d}.parq").collect()
@@ -491,8 +419,9 @@ def infer_online(prod, date, n_latest, universe, verbose):
     For prod used at 0930 of next trading day, the date in the result is the next trading day after infer_date.
     """
     import sys
-    from pit.inference import infer, InferenceMode
     from datetime import timedelta
+
+    from pit.inference import InferenceMode, infer
 
     infer_date = any2date(date)
     args = get_inference_config(prod=prod, n_latest=n_latest, universe=universe)
@@ -629,59 +558,6 @@ def infer_hist(prod, begin, end, n_latest, universe, out_dir):
     alpha.write_parquet(out_dir.joinpath(f"hist_{use_begin}_{use_end}.parq"))
 
 
-@click.command()
-def show_config():
-    """Print the configurations in the current config file."""
-    cfg = read_config()
-    click.echo(OmegaConf.to_yaml(cfg))
-
-
-@click.command()
-@click.option("--duration", default="30m", type=click.Choice(["15m", "30m", "1h", "2h", '1d', '2d', '5d']))
-def compute_slot_return(duration):
-    """compute returns for 8 intraday slots.
-    """
-    slots = ['0931', '1000', '1030', '1100', '1301', '1330', '1400', '1430']
-    times = [datetime.time(hour=int(s[:2]), minute=int(s[2:])) for s in slots]
-    cfg = read_config()
-    
-    price_dir = os.path.join(cfg.raw.dir, 'ohlcv_1m')
-    cols = ['time', 'symbol', 'adj_close']
-    pl.enable_string_cache()
-    df_price = pl.scan_parquet(price_dir + "/*.parq").select(cols).collect()
-    
-    df_price_left = df_price.filter(pl.col('time').dt.time().is_in(times))
-    df_adj = adjust_tcalendar_slot_df(duration=duration, start_slot=slots) # two columns: time, next_time
-    end_times = df_adj.select(pl.col('next_time').dt.time().unique()).to_series().to_list()
-    df_price_right = df_price.filter(pl.col('time').dt.time().is_in(end_times))
-    
-    df_merge = df_price_left.join(df_adj, on='time')
-    df_merge = df_merge.join(
-        df_price_right, 
-        left_on=['next_time', 'symbol'], 
-        right_on=['time', 'symbol'], 
-        suffix='_right'
-    )
-    
-    df_ret = df_merge.select(
-        pl.col('time').cast(pl.Date).alias('date'),
-        pl.col('time'),
-        pl.col('symbol'),
-        pl.col('adj_close_right').truediv(pl.col("adj_close")).sub(1).alias(f'ret_{duration}')
-    )
-
-    item_dir = os.path.join(cfg.derived.dir, f'ret_{duration}')
-    item_dir = Path(item_dir)
-    item_dir.mkdir(parents=True, exist_ok=True)
-    
-    # df_
-    for d, _df in df_ret.partition_by(["date"], as_dict=True).items():
-        # for (d, ), _df in df.partition_by(["date"], as_dict=True).items():
-        _df.write_parquet(f"{item_dir}/{d:%Y-%m-%d}.parq")
-    click.echo("task slot return done.")
-    
-        
-
 @click.group()
 def pit():
     """pit: Alpha Signals Generator of Pit."""
@@ -690,14 +566,10 @@ def pit():
 
 pit.add_command(train_single)
 pit.add_command(download_task)
-pit.add_command(long2widev2)
-pit.add_command(downsample10)
-pit.add_command(merge10_v2)
+pit.add_command(generate_dataset)
 pit.add_command(infer_hist)
 pit.add_command(infer_online)
 pit.add_command(update_tcalendar)
-pit.add_command(show_config)
-pit.add_command(compute_slot_return)
 
 if __name__ == "__main__":
     pit()
