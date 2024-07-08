508929bdfeed51302ad50f14f5ef09192ea19c61
wangxin <wangxin@higndimensionasset.com>
2024-07-08 10:31:30 +0800

    correct version

--- pit/__init__.py
+++ pit/__init__.py
@@ -4,5 +4,4 @@ from pit.prod import get_training_config, get_inference_config, ProdsAvailable, 
 from pit.train import TrainPipeline  # noqa
 from pit.inference import InferencePipeline  # noqa
 
-import datetime
-__version__ = datetime.datetime.now().strftime("%Y.%m.%d")
+__version__ = "2024.07.08"
